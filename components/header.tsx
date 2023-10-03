c405d5e12470648f870a9dad56cea29cea1aa8f4
cpeed <aareeb1999@gmail.com>
2023-10-03 09:44:27 +0500

    fixed header

--- components/header.tsx
+++ components/header.tsx
@@ -12,9 +12,9 @@ function Header() {
     useActiveSectionContext();
 
   return (
-    <header className=" z-50 relative">
+    <header className=" z-50  !fixed w-full h-full ">
       <motion.div
-        className=" fixed top-0 left-1/2 bg-white bg-opacity-80  shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] h-[4.5rem] w-full rounded-none border border-white border-opacity-40 sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        className=" !fixed top-0 left-1/2 bg-white bg-opacity-80  shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] h-[5.5rem] w-full rounded-none border border-white border-opacity-40 sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
